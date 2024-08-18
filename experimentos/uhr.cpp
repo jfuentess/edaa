8ca126194576ea66d9dfbef7fdffff6c74f62467
Leonardo Lovera <leonard.lover@gmail.com>
2024-08-18 16:14:17 -0400

    feat: added progress bar

--- experimentos/uhr.cpp
+++ experimentos/uhr.cpp
@@ -6,9 +6,11 @@
  * 1. Time unit: in elapsed_time,
  * 2. What to write on time_data,
  * 3. Data type and distribution of RNG,
- * 4. The experiments: in outer for loop. */
+ * 4. Additive or multiplicative stepping,
+ * 5. The experiments: in outer for loop. */
 
 #include <algorithm>
+#include <cassert>
 #include <chrono>
 #include <cmath>
 #include <cstddef>
@@ -17,10 +19,73 @@
 #include <fstream>
 #include <iostream>
 #include <random>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 // Include to be tested files here
+
+inline void validate_input(int argc, char *argv[], std::int64_t& runs,
+    std::int64_t& lower, std::int64_t& upper, std::int64_t& step)
+{
+    if (argc != 6) {
+        std::cerr << "Usage: <filename> <RUNS> <LOWER> <UPPER> <STEP>" << std::endl;
+        std::cerr << "<filename> is the name of the file where performance data will be written." << std::endl;
+        std::cerr << "It is recommended for <filename> to have .csv extension and it should not previously exist." << std::endl;
+        std::cerr << "<RUNS>: numbers of runs per test case: should be >= 32." << std::endl;
+        std::cerr << "<LOWER> <UPPER> <STEP>: range of test cases." << std::endl;
+        std::cerr << "These should all be positive." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    // Read command line arguments
+    try {
+        runs = std::stoll(argv[2]);
+        lower = std::stoll(argv[3]);
+        upper = std::stoll(argv[4]);
+        step = std::stoll(argv[5]);
+    } catch (std::invalid_argument const& ex) {
+        std::cerr << "std::invalid_argument::what(): " << ex.what() << std::endl;
+        std::exit(EXIT_FAILURE);
+    } catch (std::out_of_range const& ex) {
+        std::cerr << "std::out_of_range::what(): " << ex.what() << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    // Validate arguments
+    if (runs < 4) {
+        std::cerr << "<RUNS> must be at least 4." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    if (step <= 0 or lower <= 0 or upper <= 0) {
+        std::cerr << "<STEP>, <LOWER> and <UPPER> have to be positive." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    if (lower > upper) {
+        std::cerr << "<LOWER> must be at most equal to <UPPER>." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+inline void display_progress(std::int64_t u, std::int64_t v)
+{
+    const double progress = u / double(v);
+    const std::int64_t width = 70;
+    const std::int64_t p = width * progress;
+    std::int64_t i;
+
+    std::cout << "[";
+    for (i = 0; i < width; i++) {
+        if (i < p)
+            std::cout << "=";
+        else if (i == p)
+            std::cout << ">";
+        else
+            std::cout << " ";
+    }
+    std::cout << "] " << std::int64_t(progress * 100.0) << "%\r";
+    std::cout.flush();
+}
 
 inline void quartiles(std::vector<double>& data, std::vector<double>& q)
 {
@@ -48,7 +113,7 @@ inline void quartiles(std::vector<double>& data, std::vector<double>& q)
     }
 
     // Find lower and upper quartiles
-    if (n % 4 == 2 or n % 4 == 3) {
+    if (n % 4 >= 2) {
         q[1] = data[n / 4];
         q[3] = data[(3 * n) / 4];
     } else {
@@ -61,25 +126,14 @@ inline void quartiles(std::vector<double>& data, std::vector<double>& q)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 6) {
-        std::cerr << "Usage: <filename> <RUNS> <LOWER> <UPPER> <STEP>" << std::endl;
-        std::cerr << "<filename> is the name of the file where performance data will be written." << std::endl;
-        std::cerr << "It is recommended for <filename> to have .csv extension and it should not previously exist." << std::endl;
-        std::cerr << "<RUNS>: numbers of runs per test case: should be >= 32." << std::endl;
-        std::cerr << "<LOWER> <UPPER> <STEP>: range of test cases." << std::endl;
-        std::cerr << "These should all be positive." << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
-
-    // Set up command line arguments
+    // Validate and sanitize input
     std::int64_t runs, lower, upper, step;
-    runs = std::stoll(argv[2]);
-    lower = std::stoll(argv[3]);
-    upper = std::stoll(argv[4]);
-    step = std::stoll(argv[5]);
+    validate_input(argc, argv, runs, lower, upper, step);
 
     // Set up clock variables
-    std::int64_t n, i;
+    std::int64_t n, i, executed_runs;
+    std::int64_t total_runs_additive = runs * (((upper - lower) / step) + 1);
+    std::int64_t total_runs_multiplicative = runs * (floor(log(upper / double(lower)) / log(step)) + 1);
     std::vector<double> times(runs);
     std::vector<double> q;
     double mean_time, time_stdev, dev;
@@ -98,6 +152,7 @@ int main(int argc, char *argv[])
     time_data << "n,t_mean,t_stdev,t_Q0,t_Q1,t_Q2,t_Q3,t_Q4" << std::endl;
 
     // Begin testing
+    executed_runs = 0;
     for (n = lower; n <= upper; n += step) {
         mean_time = 0;
         time_stdev = 0;
@@ -106,6 +161,9 @@ int main(int argc, char *argv[])
 
         // Run to compute elapsed time
         for (i = 0; i < runs; i++) {
+            // Remember to change total depending on step type
+            display_progress(++executed_runs, total_runs_additive);
+
             begin_time = std::chrono::high_resolution_clock::now();
             // Function to test goes here
             end_time = std::chrono::high_resolution_clock::now();
@@ -132,6 +190,9 @@ int main(int argc, char *argv[])
         time_data << n << "," << mean_time << "," << time_stdev << ",";
         time_data << q[0] << "," << q[1] << "," << q[2] << "," << q[3] << "," << q[4] << std::endl;
     }
+
+    // This is to keep loading bar after testing
+    std::cout << std::endl;
 
     time_data.close();
 
